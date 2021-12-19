5ebbdadd4038a67aa60038e657a78e2cbe77ca61
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-12-19 16:34:22 +0300

    messages are now sent from the frontend

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,9 +1,25 @@
 import { Avatar, IconButton } from '@material-ui/core'
 import { SearchOutlined, AttachFile, MoreVert, InsertEmoticon, Mic } from '@material-ui/icons'
-import React from 'react'
+import React, { useState } from 'react'
 import './Chat.css'
+import axios from '../axios';
 
 function Chat({ messages }) {
+    const [input, setInput] = useState('');
+
+    const sendMessage = async e => {
+        e.preventDefault();
+
+        await axios.post('/api/v1/messages/new', {
+            message: input,
+            name: 'DEMO NAME',
+            timestamp: 'just now',
+            received: false,
+        });
+
+        setInput('');
+    }
+
     return (
         <div className='chat'>
             <div className='chat__header'>
@@ -39,8 +55,8 @@ function Chat({ messages }) {
             <div className='chat__footer'>
                 <InsertEmoticon />
                 <form>
-                    <input placeholder='Type a message' type='text'/>
-                    <button type='submit' >Send a message</button>
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder='Type a message' type='text'/>
+                    <button onClick={sendMessage} type='submit' >Send a message</button>
                 </form>
                 <Mic />
             </div>
